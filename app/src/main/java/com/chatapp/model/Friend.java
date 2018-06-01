8699b1d6d85e957b0cd536bcfdf856342774fe57
pata02525 <hata02525@gmail.com>
2018-06-01 17:43:13 +0530

    fixed FCM Chat one to one

---
+++ app/src/main/java/com/chatapp/model/Friend.java
@@ -0,0 +1,29 @@
+package com.chatapp.model;
+
+import com.chatapp.data.ParseFirebaseData;
+
+import java.io.Serializable;
+
+public class Friend implements Serializable {
+	private String id;
+	private String name;
+	private String photo;
+
+	public Friend(String id, String name, String photo) {
+		this.id = id;
+		this.name = name;
+		this.photo = photo;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+}
